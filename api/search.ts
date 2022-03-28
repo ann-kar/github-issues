26c0fa3e9c35dced869baed41ee8e16e21e7662f
ann-kar <anna.paulina.karpiuk@gmail.com>
2022-03-28 14:04:05 +0200

    Added API call

--- api/search.ts
+++ api/search.ts
@@ -2,6 +2,9 @@ import { ISearch, ISearchRequest, ISearchResponse } from "../interfaces/search";
 
 export class Search implements ISearch {
   async search(request: ISearchRequest): Promise<ISearchResponse> {
-    throw new Error("Method not implemented.");
+    const response = await fetch(
+      'http://localhost:3000/api/search'
+    );
+    return await response.json();
   }
 }
