d12f339aac0ea6d2b2495e81d1a615037139686c
ann-kar <anna.paulina.karpiuk@gmail.com>
2022-03-28 22:10:37 +0200

    Fixed avatar display

--- components/User.tsx
+++ components/User.tsx
@@ -4,7 +4,7 @@ import styles from "../styles/Home.module.css";
 export const User = ({ item }: { item: IUser }) => {
   return (
     <>
-      <img className={styles.listItemAvatar}>{item.avatar}</img>
+      <img className={styles.listItemAvatar} src={item.avatar}></img>
       <h2 className={styles.listItemTitle}>{item.name}</h2>
       <h2 className={styles.listItemTitle}>{item.username}</h2>
       <h3 className={styles.listItemLocation}>{item.location}</h3>
