9145eb62795a140288a4a45c8ada6c48f022b588
Michał Warda <michal.warda@elpassion.pl>
2022-03-28 11:29:46 +0200

    Add GithubClient.getUsers without params

--- test/GithubClient.test.ts
+++ test/GithubClient.test.ts
@@ -1,14 +1,18 @@
-import { expect } from "@jest/globals";
+import fetch from "node-fetch";
 import { GithubUser } from "../interfaces/github";
+import { withRecording } from "./withRecording";
 
 class GithubClient {
   async getUsers(): Promise<GithubUser[]> {
-    return [];
+    const response = await fetch("https://api.github.com/users");
+    return (await response.json()) as GithubUser[];
   }
 }
 
 describe("#getUsers", function () {
   test("returns github users", async () => {
-    await expect(new GithubClient().getUsers()).resolves.toEqual([]);
+    return withRecording(__dirname, "users", async () => {
+      await expect(new GithubClient().getUsers()).resolves.toMatchSnapshot();
+    });
   });
 });
