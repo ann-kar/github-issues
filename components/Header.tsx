f53f99e4e10145a5d6591cf3bea6dd8cc0a021c6
ann-kar <anna.paulina.karpiuk@gmail.com>
2022-03-29 10:29:41 +0200

    Set children to optional

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,5 @@
 import styles from "../styles/Home.module.css";
 
-export const Header = ({ children }: { children: JSX.Element }) => {
+export const Header = ({ children }: { children?: JSX.Element }) => {
   return <header className={styles.header}>{children}</header>;
 };
