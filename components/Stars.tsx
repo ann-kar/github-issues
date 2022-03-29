b176a69b9b90ce2a23f4fda3719f77b3c6206fb5
ann-kar <anna.paulina.karpiuk@gmail.com>
2022-03-29 18:39:48 +0200

    Fixed svg path

--- components/Stars.tsx
+++ components/Stars.tsx
@@ -3,7 +3,7 @@ import styles from "../styles/Home.module.css";
 export const Stars = ({ count }: { count: number }) => {
   return (
     <span className={styles.star}>
-      <img className={styles.starIcon} src="./assets/star.svg"></img>
+      <img className={styles.starIcon} src="/assets/star.svg"></img>
       <span>{count}</span>
     </span>
   );
